8293185fcc4193f0578f82e468cc320320f65e0e
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-07-13 15:20:27 +0200

    Add Redux for list

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
+// import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
-  state = {
-    cards: this.props.cards || []
-  }
+  // state = {
+  //   cards: this.props.cards || [],
+  // }
 
   static propTypes = {
     title: PropTypes.node.isRequired,
@@ -21,44 +21,44 @@ class Column extends React.Component {
     desciption: settings.cardCreatorText,
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title,
-          }
-        ]
-      }
-    ));
-  }
+  // addCard(title){
+  //   this.setState(state => (
+  //     {
+  //       cards: [
+  //         ...state.cards,
+  //         {
+  //           key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+  //           title,
+  //         },
+  //       ],
+  //     }
+  //   ));
+  // }
 
   render() {
+    const {title, icon, cards} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
           <span className={styles.icon}>
             <Icon 
-              name={this.props.icon}
+              name={icon}
             />
-          </span>{this.props.title}
+          </span>{title}
         </h3>
         <div>
-          {this.state.cards.map(({key, ...cardsProps}) => (
-            <Card key={key} {...cardsProps} />
+          {cards.map((cardData) => (
+            <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        <div>
+        {/* <div>
           <Creator 
             text={settings.cardCreatorText} 
             action={title => this.addCard(title)}
           />
-        </div>
+        </div> */}
       </section>
-      
-    )
+    );
   }
 }
 
--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -6,6 +6,10 @@ export const settings = {
     buttonCancel: 'Cancel',
     defaultText: 'Add new item',
   },
+  search: {
+    defaultText: 'Search...',
+    icon: 'search',
+  },
   defaultListDescription: '<p>I can do all the things!!!</p>',
   defaultColumnIcon: 'list-alt',
 };
@@ -17,7 +21,7 @@ export const pageContents = {
 
 export const listData = {
   title: 'Things to do <sup>soon!</sup>',
-  description: 'Interesting things that I planned!',
+  description: 'Interesting things I want to check out!',
   image: '../src/images/image.jpg',
   columns: [
     {
@@ -27,11 +31,11 @@ export const listData = {
       cards: [
         {
           key: 0,
-          title: 'AAAAA',
+          title: 'This Is Going to Hurt',
         },
         {
           key: 1,
-          title: 'BBBBB',
+          title: 'Interpreter of Maladies',
         },
       ],
     },
@@ -42,11 +46,11 @@ export const listData = {
       cards: [
         {
           key: 0,
-          title: 'CCCCC',
+          title: 'To-do app',
         },
         {
           key: 1,
-          title: 'DDDDD',
+          title: 'Portfolio',
         },
       ],
     },
@@ -57,13 +61,91 @@ export const listData = {
       cards: [
         {
           key: 0,
-          title: 'EEEEE',
+          title: '-',
         },
         {
           key: 1,
-          title: 'FFFFF',
+          title: '-',
         },
       ],
     },
   ],
 };
+
+const lists = [
+  {
+    id: 'list-1',
+    title: 'Things to do <sup>soon!</sup>',
+    description: 'Interesting things I want to check out!',
+    image: '../src/images/image.jpg',
+  },
+]; 
+
+const columns = [
+  {
+    id: 'column-1',
+    listId: 'list-1',
+    title: 'Books',
+    icon: 'book',
+  },
+  {
+    id: 'column-2',
+    listId: 'list-1',
+    title: 'Projects',
+    icon: 'laptop-code',
+  },
+  {
+    id: 'column-3',
+    listId: 'list-1',
+    title: 'Articles',
+    icon: 'newspaper',
+  },
+  {
+    id: 'column-4',
+    listId: 'list-2',
+    title: 'Strange column',
+    icon: 'question',
+  },
+];
+
+const cards = [
+  {
+    id: 'card-1',
+    columnId: 'column-1',
+    title: 'This Is Going to Hurt',
+  },
+  {
+    id: 'card-2',
+    columnId: 'column-1',
+    title: 'Interpreter of Maladies',
+  },
+  {
+    id: 'card-3',
+    columnId: 'column-2',
+    title: 'To-do app',
+  },
+  {
+    id: 'card-4',
+    columnId: 'column-2',
+    title: 'Portfolio',
+  },
+  {
+    id: 'card-5',
+    columnId: 'column-3',
+    title: '-',
+  },
+  {
+    id: 'card-6',
+    columnId: 'column-3',
+    title: '-',
+  },
+];
+
+const initialStoreData = {
+  app: {...pageContents},
+  lists: [...lists],
+  columns: [...columns],
+  cards: [...cards],
+};
+
+export default initialStoreData;
